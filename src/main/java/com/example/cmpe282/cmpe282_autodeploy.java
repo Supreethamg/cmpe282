77d31069a77e8fd19ab71bb0d8fd20ca1ed527d4
Supreetha M G <31361767+Supreethamg@users.noreply.github.com>
2018-03-18 23:03:24 -0700

    Add files via upload

---
+++ src/main/java/com/example/cmpe282/cmpe282_autodeploy.java
@@ -0,0 +1,20 @@
+package com.example.cmpe282;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class cmpe282_autodeploy {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello BackBenchers@Cloud!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(cmpe282_autodeploy.class, args);
+    }
+
+}
