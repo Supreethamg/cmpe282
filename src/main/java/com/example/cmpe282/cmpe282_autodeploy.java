50c8bf7ee939a8907f62cd4b4f3fa7261f1032cf
Supreetha M G <31361767+Supreethamg@users.noreply.github.com>
2018-03-25 14:57:11 -0700

    Update cmpe282_autodeploy.java

--- src/main/java/com/example/cmpe282/cmpe282_autodeploy.java
+++ src/main/java/com/example/cmpe282/cmpe282_autodeploy.java
@@ -10,7 +10,7 @@ public class cmpe282_autodeploy {
 
     @RequestMapping("/")
     String home() {
-        return "Hello!";
+        return "Hello back!";
     }
 
     public static void main(String[] args) throws Exception {
