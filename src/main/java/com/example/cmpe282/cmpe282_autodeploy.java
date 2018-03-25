b816e5a7008707e55dd9de93e04dbf3e1713afc7
Supreetha M G <31361767+Supreethamg@users.noreply.github.com>
2018-03-25 14:23:10 -0700

    Update cmpe282_autodeploy.java

--- src/main/java/com/example/cmpe282/cmpe282_autodeploy.java
+++ src/main/java/com/example/cmpe282/cmpe282_autodeploy.java
@@ -10,7 +10,7 @@ public class cmpe282_autodeploy {
 
     @RequestMapping("/")
     String home() {
-        return "Hello BackBenchers@cloud";
+        return "Hello BackBenchers@cloud!!";
     }
 
     public static void main(String[] args) throws Exception {
